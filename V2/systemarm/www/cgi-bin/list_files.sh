318f15534e3b1a25a20575a42ab8be05065a11c6
flfroehl <flfroehl@htwg-konstanz.de>
2013-11-11 12:29:07 +0100

    fehler im script behoben

--- V2/systemarm/www/cgi-bin/list_files.sh
+++ V2/systemarm/www/cgi-bin/list_files.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
 
+
 tmp_path="/home/flfroehl/tmp"
 
-echo "Files and Folders in $tmp_path"
+
+echo "Content-type: text/html"
+echo ""
+echo '<!DOCTYPE html>'
+echo '<head>'
+echo '<title>System Informations</title>'
+echo '<link rel="stylesheet" href="../style.css"/>'
+echo '</head>' 
+echo '<body>'
+echo "<h1>Files and Folders in $tmp_path</h1>"
 ls -ag $tmp_path
+echo '</body>'
+echo '</html>'
 exit 0
--- V2/systemarm/www/cgi-bin/rm_tmp.sh
+++ V2/systemarm/www/cgi-bin/rm_tmp.sh
@@ -2,7 +2,17 @@
 
 tmp_path="/home/flfroehl/temp"
 
-echo "Trying to remove Tempfiles! ( $tmp_path )"
+echo "Content-type: text/html"
+echo ""
+echo '<!DOCTYPE html>'
+echo '<head>'
+echo '<title>System Informations</title>'
+echo '<link rel="stylesheet" href="../style.css"/>'
+echo '</head>' 
+echo '<body>'
+echo "<h1>Trying to remove Tempfiles! ( $tmp_path )</h1>"
 rm -rf $tmp_path/*
+echo '</body>'
+echo '</html>'
 exit 0
 
