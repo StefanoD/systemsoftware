6914b14c8a74ace84f98e0d829daa32546a3ac5f
robin <roroettg@gmail.com>
2013-11-19 17:50:00 +0100

    adding timeinfo

---
+++ V3/timeinfo.c
@@ -0,0 +1,20 @@
+#include <sys/sysinfo.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+
+int main() {
+	struct timeval t1;
+	struct timeval t2;
+	gettimeofday(&t1,NULL);
+	
+	while(1) {
+		gettimeofday(&t2,NULL);
+		long res = t2.tv_sec - t1.tv_sec;
+		printf("Zeit: %ld\n", res);
+		fflush(stdout);
+		sleep(1);
+	}
+	
+}
