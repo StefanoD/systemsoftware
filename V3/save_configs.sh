da3c93663a98c9c1a03dcc5f8dc61d2953a27e05
flfroehl <flfroehl@htwg-konstanz.de>
2013-12-04 14:39:55 +0100

    delete redundant files

--- V3/save_configs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo "\n\tCopy Configs!"
-cp .config ../../systemsoftware/V3/br.config
-cp output/build/linux-3.10.7/.config ../../systemsoftware/V3/k.config
-cp output/build/busybox-1.21.1/.config ../../systemsoftware/V3/bb.config
-echo "\tdone!\n"
-
-
